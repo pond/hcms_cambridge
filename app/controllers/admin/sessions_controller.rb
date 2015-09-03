0f6147409edf2c88fbc4c212a42aa1b768a14668
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2015-09-03 21:10:59 +1200

    Security improvement

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,4 +3,13 @@ class Admin::SessionsController < ::Devise::SessionsController
 
   # Via Devise
   before_action :authenticate_admin_user!
+
+  # By sleeping a few seconds before any sign-in attempt, we delay humans
+  # wanting to sign in, but not very much; more importantly, we dramatically
+  # reduce the throughput possible for brute force attacks on the page.
+  #
+  def create
+    sleep(5)
+    super
+  end
 end
