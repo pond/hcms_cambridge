2b76854ae385a61f5ba0bec0ebd3ea58123b69ad
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2016-04-02 10:30:21 +1300

    Bundle update everything because of a Fog breakage; suddenly the asset pipeline (ARGH) blew up and insisted on new additions to configuration.

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Hcms
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w( pages.css pages.js admin.css admin.js )
   end
 end
