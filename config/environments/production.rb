a4e73502bad5fcfd015911665a5d2db83fbd00d3
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2019-06-13 18:01:54 +1200

    Fix typing error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Heroku precompiles assets upon deployment.
-  config.asset.compile = false
+  config.assets.compile = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
