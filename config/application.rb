a8699ec5d197ca86fbc29b89e1e89dc0cd8b8021
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2020-04-29 11:51:39 +1200

    Fix an execution order issue that prevented asset compilation in Production

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Hcms
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Load this here so it's available for config/environments/* - if we did this
+    # instead in a config/initializers/* file, it wouldn't be executed in time.
+    #
+    Rails.application.config.uk_org_pond_hcms = OpenStruct.new( YAML.load_file( Rails.root.join( 'config' ).join( 'config.yml' ) ) )
+
   end
 end
