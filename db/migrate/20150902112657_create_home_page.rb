96359527e6ff19d8e225edb0149abee6357c89e4
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2020-04-29 22:47:54 +1200

    Fix a historical migration & rebuild schema

--- db/migrate/20150902112657_create_home_page.rb
+++ db/migrate/20150902112657_create_home_page.rb
@@ -1,6 +1,13 @@
 class CreateHomePage < ActiveRecord::Migration[4.2]
   def up
-    Page.create :title => 'Home', :body => 'Welcome!' unless Page.count > 0
+    unless Page.count > 0
+      page = Page.new :title => 'Home', :body => 'Welcome!'
+
+      # Skip validation to avoid validations based on columns only added in
+      # later migrations, from stopping this running now.
+      #
+      page.save(validate: false)
+    end
   end
 
   def down
