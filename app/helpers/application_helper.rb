c1b4b99561e34334ee602048516ae8f6eace135a
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2020-05-03 16:04:45 +1200

    First version of blogs completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def apphelp_human_time(datetime)
+    current_year = Time.now.in_time_zone('London').year
+    local_time   = datetime.in_time_zone('London')
+    formatter    = current_year == local_time.year ? :short_no_year : short
+
+    l(local_time, format: formatter)
+  end
 end
