cc08034914f8d87ed8111d09d6107347fd0b9d41
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2020-05-03 23:50:34 +1200

    Implement previously-overlooked public 'show' action for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,8 @@ class ArticlesController < ApplicationController
 
   layout 'articles'
 
-  IS_INTEGER = /\A\d+\z/
-
   def show
-    if params[:id].nil?
-      @article = Article.first # See default_scope in article.rb
-    else
-      @article = Article.find_by_id_or_slug!( params[ :id ] )
-    end
+    @page    =    Page.find_by_id_or_slug!( params[ :page_id ] )
+    @article = Article.find_by_id_or_slug!( params[ :id      ] )
   end
 end
