c0a2fd158e30ecf396eefba3c2456804dd416faa
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-08-01 17:07:15 +1200

    Configuration aimed at Heroku

---
+++ config/puma.rb
@@ -0,0 +1,18 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+# Support IPv6 by binding to host `::` instead of `0.0.0.0`
+port(ENV['PORT'] || 3000, "::")
+
+# Router keepalive idle timeout + 5 seconds
+persistent_timeout(95)
+
+# Turn off keepalive support for better long tails response time with Router 2.0
+# Remove this line when https://github.com/puma/puma/issues/3487 is closed, and the fix is released
+enable_keep_alives(false) if respond_to?(:enable_keep_alives)
+
+rackup      DefaultRackup if defined?(DefaultRackup)
+environment ENV['RACK_ENV'] || 'development'
