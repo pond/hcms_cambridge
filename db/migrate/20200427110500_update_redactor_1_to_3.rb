db7cb55204cb3aa22aa33f3a4ea08f24f931cf2f
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-08-07 08:36:17 +1200

    Fix up old migration which never ran

--- db/migrate/20200427110500_update_redactor_1_to_3.rb
+++ db/migrate/20200427110500_update_redactor_1_to_3.rb
@@ -1,16 +1,7 @@
 class UpdateRedactor1To3 < ActiveRecord::Migration[5.2]
-  def self.up
-
-    # Table name may be unexpected, depending on exact Redactor setup.
-    #
-    rename_table :redactor2_assets, :redactor_assets rescue nil
-
+  def change
     change_table :redactor_assets do |t|
       t.string :custom_file_name
     end
-  end
-
-  def self.down
-    raise ActiveRecord::IrreversibleMigration
   end
 end
