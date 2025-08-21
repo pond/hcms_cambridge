c41061c52e83842f59059c9c3eea9c819ecfb70a
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-08-21 14:52:44 +1200

    Update Production configuration for Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,19 +62,30 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "hcms_#{Rails.env}"
 
-  config.action_mailer.perform_caching       = false
-  config.action_mailer.perform_deliveries    = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method       = :smtp
-  config.action_mailer.smtp_settings         = {
-    :address              => 'smtp.sendgrid.net',
-    :port                 => '587',
-    :enable_starttls_auto => true,
-    :user_name            => ENV[ 'SENDGRID_USERNAME' ],
-    :password             => ENV[ 'SENDGRID_PASSWORD' ],
-    :domain               => Rails.application.config.uk_org_pond_hcms.mail_domain,
-    :authentication       => :plain
-  }
+  config.action_mailer.default_url_options = { host: Rails.application.config.uk_org_pond_hcms.mail_domain, port: 443 }
+
+  if ENV['MAILGUN_API_KEY'].present?
+    # For MailGun - visit Heroku, go to app resources, open Mailgun from there
+    config.action_mailer.delivery_method  = :mailgun
+    config.action_mailer.mailgun_settings = {
+      api_key: ENV['MAILGUN_API_KEY'],
+      domain:  ENV['MAILGUN_DOMAIN']
+    }
+  else
+    config.action_mailer.perform_caching       = false
+    config.action_mailer.perform_deliveries    = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method       = :smtp
+    config.action_mailer.smtp_settings         = {
+      :address              => 'smtp.sendgrid.net',
+      :port                 => '587',
+      :enable_starttls_auto => true,
+      :user_name            => ENV[ 'SENDGRID_USERNAME' ],
+      :password             => ENV[ 'SENDGRID_PASSWORD' ],
+      :domain               => Rails.application.config.uk_org_pond_hcms.mail_domain,
+      :authentication       => :plain
+    }
+  end
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
