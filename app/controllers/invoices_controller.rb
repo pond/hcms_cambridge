fa14fc41c884e783586bda4ac872006f0af362f6
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-11-06 22:44:36 +1300

    Let admins generate invoices; add a 'print' button

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -6,6 +6,16 @@ class InvoicesController < OrdersController
   include GetOrderCarefullyConcern # Sets @order
 
   def show
+
+    # Tidy up URLs from "use a GET-based from to get standard button styling"
+    # hacks. We wouldn't care so much except printed output tends to include
+    # the page URL.
+    #
+    if request.url.end_with?('?')
+      redirect_to(request.url.chomp('?'))
+      return
+    end
+
     render()
   end
 end
